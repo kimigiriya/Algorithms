bb9039ecc48d272f21697c9eca1a676e18fc4b09
kimigiriya <kimigiriya@gmail.com>
2024-11-29 18:16:20 +0700

    Add files via upload

---
+++ Algorithms/Algorithms.cpp
@@ -0,0 +1,84 @@
+﻿#include "Algorithms.h"
+#include <iostream>
+#include <fstream>
+#include <locale.h>
+#include <locale>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include <sstream>
+#include <cstring>
+#include <string>
+#include <codecvt>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251 >> null");
+
+	try {
+		std::vector<int> prime = ElementsFromFile<int>("PrimeNumbers.txt");
+		std::cout << "Исходные элементы:\n";
+		Printv(prime);
+
+		int n;
+		std::cout << "Введите n: ";
+		std::cin >> n;
+		int countN = count(prime.begin(), prime.end(), n);
+		if (countN > 0) {
+			std::cout << "Элемент " << n << " встречается " << countN << " раз" << "\n";
+			std::cout << "Позиция элементов " << n << ": ";
+			PositionOfElements(prime, n);
+		}
+		else {
+			std::cout << "Элемент " << n << " не найден в файле\n";
+		}
+        
+		int evenCount = EvenElements(prime);
+		std::cout << "Количество четных элементов: " << evenCount << "\n";
+		int oddCount = OddElements(prime);
+		std::cout << "Количество нечетных элементов: " << oddCount << "\n";
+		std::cout << "Позиция четных элементов " << n << ": ";
+		PositionOfEvenElements(prime);
+		std::cout << "Позиция нечетных элементов " << n << ": ";
+		PositionOfOddElements(prime);
+		int sum = 0;
+		if (evenCount > oddCount) {
+			std::cout << "Кол-во четный элементов больше, чем нечётных элементов\n";
+			sum = SumOfEvenElements(prime);
+			printf("Сумма всех чётных элементов: %d\n", sum);
+		}
+		else {
+			std::cout << "Кол-во четный элементов равно или меньше, чем нечётных элементов\n";
+			sum = SumOfEvenElements(prime) + SumOfOddElements(prime);
+			printf("Сумма всех элементов: %d\n", sum);
+		}
+
+		std::vector<int> sorty = prime;
+		std::sort(sorty.begin(), sorty.end());
+		std::cout << "Отсортирированные элементы:\n";
+		Printv(sorty);
+
+		std::cout << "Перестановка первого элемента и последнего элемента\n";
+		if (prime.size() > 1) {	
+			std::swap(prime.front(), prime.back());
+			std::cout << "Переставленные элементы:\n";
+			Printv(prime);
+		}
+		else if (prime.size() == 1) {
+			std::cout << "Вектор содержит только один элемент, перестановка невозможна\n";
+		}
+		else {
+			std::cout << "Вектор пуст, перестановка невозможна\n";
+		}
+	}
+	catch (const std::runtime_error& error) {
+		std::cerr << error.what() << std::endl;
+	}
+	catch (const std::invalid_argument& error) {
+		std::cerr << "Ошибка: " << error.what() << std::endl;
+	}
+
+	return 0;
+}
---
+++ Algorithms/Algorithms.h
@@ -0,0 +1,169 @@
+﻿#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <locale.h>
+#include <locale>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include <sstream>
+#include <cstring>
+#include <string>
+#include <codecvt>
+
+template<typename T>
+std::vector<T> ElementsFromFile(const std::string& nameOfFile)
+{
+    std::ifstream File(nameOfFile, std::ios::in);
+    std::vector<T> elements;
+    if (File.is_open()) {
+        File.imbue(std::locale("rus_rus.1251"));
+        T num;
+        while (File >> num) {
+            elements.emplace_back(num);
+        }
+        if (File.fail() and !File.eof()) {
+            throw std::runtime_error("Ошибка чтения из файла " + nameOfFile);
+        }
+    }
+    else {
+        throw std::runtime_error ("Ошибка открытия файла " + nameOfFile);
+    }
+    File.close();
+    return elements;
+}
+
+template<typename T>
+void Printv(const std::vector<T>& elements)
+{
+    std::locale loc("ru_RU.UTF-8");
+    std::cout.imbue(loc);
+    for (const T& value : elements)
+        std::cout << value << " ";
+    std::cout << "\n";
+}
+
+template<typename T>
+void PositionOfElements(const std::vector<T>& elements, T n)
+{
+    auto pseudo = elements.begin();
+    while (pseudo != elements.end()) {
+        pseudo = std::find(pseudo, elements.end(), n);
+        if (pseudo != elements.end()) {
+            std::cout << std::distance(elements.begin(), pseudo) << " ";
+            pseudo++;
+        }
+        else {
+            break;
+        }
+    }
+    std::cout << "\n";
+}
+
+template<typename T>
+int EvenElements(const std::vector<T>& elements)
+{
+    return std::count_if(
+        elements.begin(),
+        elements.end(),
+        [](int x) { return x % 2 == 0; });
+}
+
+template <>
+int EvenElements<std::string>(const std::vector<std::string>& elements) {
+    throw std::invalid_argument("Нельзя проверять четность строк");
+}
+
+template<typename T>
+int OddElements(const std::vector<T>& elements)
+{
+    return std::count_if(
+        elements.begin(),
+        elements.end(),
+        [](int x) { return x % 2 != 0; });
+}
+
+template <>
+int OddElements<std::string>(const std::vector<std::string>& elements) {
+    throw std::invalid_argument("Нельзя проверять нечетность строк");
+}
+
+template<typename T>
+void PositionOfEvenElements(const std::vector<T>& elements)
+{
+    auto pseudo = elements.begin();
+    while (pseudo != elements.end()) {
+        pseudo = std::find_if(pseudo, elements.end(), [](const T& value) { return value % 2 == 0; });
+        if (pseudo != elements.end()) {
+            std::cout << std::distance(elements.begin(), pseudo) << " ";
+            pseudo++;
+        }
+        else {
+            break;
+        }
+    }
+    std::cout << "\n";
+}
+
+template <>
+void PositionOfEvenElements<std::string>(const std::vector<std::string>& elements) {
+    throw std::invalid_argument("Нельзя проверять четность строк");
+}
+
+template<typename T>
+void PositionOfOddElements(const std::vector<T>& elements)
+{
+    auto pseudo = elements.begin();
+    while (pseudo != elements.end()) {
+        pseudo = std::find_if(pseudo, elements.end(), [](const T& value) { return value % 2 != 0; });
+        if (pseudo != elements.end()) {
+            std::cout << std::distance(elements.begin(), pseudo) << " ";
+            pseudo++;
+        }
+        else {
+            break;
+        }
+    }
+    std::cout << "\n";
+}
+
+template <>
+void PositionOfOddElements<std::string>(const std::vector<std::string>& elements) {
+    throw std::invalid_argument("Нельзя проверять нечетность строк");
+}
+
+template<typename T>
+int SumOfEvenElements(const std::vector<T>& elements)
+{
+    int sum = 0;
+    for (int i = 0; i < elements.size(); ++i) {
+        if (elements[i] % 2 == 0) {
+            sum += elements[i];
+        }
+    }
+    return sum;
+}
+
+template <>
+int SumOfEvenElements<std::string>(const std::vector<std::string>& elements) {
+    throw std::invalid_argument("Нельзя проверять четность строк");
+}
+
+template<typename T>
+int SumOfOddElements(const std::vector<T>& elements)
+{
+    int sum = 0;
+    for (int i = 0; i < elements.size(); ++i) {
+        if (elements[i] % 2 != 0) {
+            sum += elements[i];
+        }
+    }
+    return sum;
+}
+
+template <>
+int SumOfOddElements<std::string>(const std::vector<std::string>& elements) {
+    throw std::invalid_argument("Нельзя проверять нечетность строк");
+}
